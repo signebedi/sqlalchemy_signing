9d964926350a318be28b4a4afcb3038f0242fdd6
signebedi <signe@siftingwinnowing.com>
2024-03-31 11:34:50 -0500

    Modified: >v.1.0.5 (#15)

--- sqlalchemy_signing/__metadata__.py
+++ sqlalchemy_signing/__metadata__.py
@@ -1,7 +1,7 @@
 __name__ = "sqlalchemy_signing"
 __author__ = "Sig Janoska-Bedi"
 __credits__ = ["Sig Janoska-Bedi",]
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __license__ = "BSD-3-Clause"
 __maintainer__ = "Sig Janoska-Bedi"
 __email__ = "signe@atreeus.com"
